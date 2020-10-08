1351eb8b0f89ca197f4c82e6a0edb9e768f86067
yuridido <66362431+yuridido@users.noreply.github.com>
2020-10-08 16:06:32 +0200

    schema base

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <img src="" alt="">
+    <nav>
+        <ul>
+            <li> <a href="{{route('home')}}">Home</a> </li>
+            <li> <a href="{{route('prodotti')}}">Prodotti</a> </li>
+            <li> <a href="{{route('contatti')}}">Contatti</a> </li>
+        </ul>
+    </nav>
+</header>
