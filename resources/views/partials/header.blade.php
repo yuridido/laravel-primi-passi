0af0040d40d5547d53d35745922dfe5f201e7024
yuridido <66362431+yuridido@users.noreply.github.com>
2020-10-08 16:49:52 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <img src="" alt="">
+    <img src="https://cdn.shortpixel.ai/client/q_lossy,ret_img/https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="logo">
     <nav>
         <ul>
             <li> <a href="{{route('home')}}">Home</a> </li>
